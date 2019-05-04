6210d1b50d7729802ec59fbc787a3190e5448793
Harry Zhao <zhaoenxiao@126.com>
2019-05-04 17:04:48 +0800

    修复了一个 bug 和修改了动画的代码
    1.使用`TransitionGroup`来帮助实现每一项的出场动画。
    2.修复了当从搜索页面新添加一本书到书架页面时并没有添加的问题
    3.在笔记中添加了新遇到的问题

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import * as BooksAPI from "./BooksAPI"
 import "./App.css"
 import BookListPage from "./pages/BookListPage"
 import SearchPage from "./pages/SearchPage"
-import { Route } from "react-router-dom"
+import { Route, withRouter } from "react-router-dom"
 import { CSSTransition } from "react-transition-group"
+import uuid from "uuid"
 
 class BooksApp extends React.Component {
   state = {
@@ -14,38 +15,46 @@ class BooksApp extends React.Component {
   }
 
   componentDidMount() {
+    console.log("uuid", uuid.v4())
     //获取默认的书籍对象数组
     BooksAPI.getAll().then(books => {
       this.setState({
         books
+      },function(){
+        this.state.books.forEach(book => {
+          // BooksAPI.update(book, 'none') // 用于清空数据
+        })
       })
     })
+    
   }
 
   changeShelf = (e, book) => {
     let i = e.target.selectedIndex
     let v = e.target.options[i].value
     // 先利用本地数据进行更新并渲染，这样能提高交互的即时性，因为不用等待服务器的返回
-    // 书架页面书籍信息的本地更新
-    this.setState(state => ({
-      books: state.books.map(b => {
-        if (b.id === book.id) {
-          b.shelf = v
-        }
-        return b
-      })
-    }))
-
-    // 搜索页面添加到本地数据
-    let b = this.state.books
-    // 判断书架中是否有这本书，没有才添加，有的话 只是改变其在书架中的位置
+    let existingBooks = this.state.books
     // 使用 includes 方法判断是否已经存在这本书
-    let hasThisBook = b.map(b => b.id).includes(book.id)
-
-    if (!hasThisBook) {
-      b.cancat(book)
-      this.setState({
-        books: b
+    let hasThisBook = existingBooks.map(b => b.id).includes(book.id)
+    // 书架页面书籍信息的本地更新
+    if (hasThisBook) {
+      this.setState(state => ({
+        books: state.books.map(b => {
+          if (b.id === book.id) {
+            b.shelf = v
+          }
+          return b
+        })
+      }))
+    } else {
+      // 搜索页面添加到主页面
+      this.setState(state => {
+        const books = JSON.parse(JSON.stringify(state.books))
+        book.shelf = v
+        books.push(book)
+        return {
+          books: books
+        }
       })
     }
 
@@ -55,7 +64,6 @@ class BooksApp extends React.Component {
 
   handlerSearch = query => {
     BooksAPI.search(query).then(books => {
-      debugger
       // 如果搜索获得的结果有书籍则为搜索出来的书匹配书架上的书，并为其添加书架属性，这样便可以同步显示其书架信息
       if (books && books.length) {
         this.setState(state => ({
@@ -103,6 +111,7 @@ class BooksApp extends React.Component {
                 onSearch={this.handlerSearch}
                 onChangeShelf={(e, book) => {
                   this.changeShelf(e, book)
+                  // 回到主页面
                   history.push("/")
                   // 清除 search 页面中的书籍
                   this.setState({
@@ -111,7 +120,8 @@ class BooksApp extends React.Component {
                 }}
                 onCleanSearch={() => {
                   this.setState({
-                    searchBooks: []
+                    searchBooks: [],
+                    isHint: false
                   })
                 }}
                 books={this.state.searchBooks}
--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -1,27 +1,29 @@
 import React from "react"
 import Book from "./Book"
-import { CSSTransition } from "react-transition-group"
+import { CSSTransition, TransitionGroup } from "react-transition-group"
 
 let BookShelf = ({ books, onChangeShelf, shelfTitle }) => (
   <div className="bookshelf">
     <h2 className="bookshelf-title">{shelfTitle}</h2>
     <div className="bookshelf-books">
-      <ol className="books-grid">
-        {books.length === 0
-          ? "This book shelf has no books"
-          : books.map((book, i) => (
-              <CSSTransition timeout={300} classNames="alert" in={true} key={i}>
-                <Book
-                  url={book.imageLinks ? book.imageLinks.smallThumbnail : ""}
-                  title={book.title}
-                  author={book.authors}
-                  shelf={book.shelf}
-                  book={book}
-                  onChangeShelf={onChangeShelf}
-                />
-              </CSSTransition>
-            ))}
-      </ol>
+      {books.length === 0 ? (
+        "This book shelf has no books"
+      ) : (
+        <TransitionGroup component="ol" className="books-grid">
+          {books.map((book) => (
+            <CSSTransition timeout={300} classNames="alert" key={book.id}>
+              <Book
+                url={book.imageLinks ? book.imageLinks.smallThumbnail : ""}
+                title={book.title}
+                author={book.authors}
+                shelf={book.shelf}
+                book={book}
+                onChangeShelf={onChangeShelf}
+              />
+            </CSSTransition>
+          ))}
+        </TransitionGroup>
+      )}
     </div>
   </div>
 )
